61a525e029f451091951616f28287018b70e59f8
David <4292@holbertonschool.com>
2022-12-01 18:24:26 -0400

    calculate shape without matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,13 +4,14 @@
 
 def matrix_shape(matrix):
     """ Calculates shape of a matrix """
-    try:
-        shape = [len(matrix), len(matrix[0]), len(matrix[0][0])]
-        return shape
-    except TypeError:
+    if matrix:
         try:
-            shape = [len(matrix), len(matrix[0])]
+            shape = [len(matrix), len(matrix[0]), len(matrix[0][0])]
             return shape
         except TypeError:
-            shape = [len(matrix)]
-            return shape
+            try:
+                shape = [len(matrix), len(matrix[0])]
+                return shape
+            except TypeError:
+                shape = [len(matrix)]
+                return shape
