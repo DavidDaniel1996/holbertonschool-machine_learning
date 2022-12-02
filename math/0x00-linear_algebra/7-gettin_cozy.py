8f5ae48c13802c0c63bd65e8a18ea7590315d194
David <4292@holbertonschool.com>
2022-12-02 14:58:19 -0400

    concatenate 2d matrices along specified axis

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module concatenates two 2d matrices """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+
+    new_matrix = []
+
+    for i in range(len(mat1)):
+        new_row = []
+        for j in range(len(mat1[i])):
+            new_row.append(mat1[i][j])
+        new_matrix.append(new_row)
+
+    if axis == 0:
+        for i in range(len(mat2)):
+            new_matrix.append(mat2[i][:])
+
+        return new_matrix
+
+    elif axis == 1:
+        for i in range(len(mat2)):
+            for j in range(len(mat2[i])):
+                new_matrix[i].append(mat2[i][j])
+
+        return new_matrix
+
+    else:
+        return None
