42077d404fd5e62f93d21c14b6fc5bfeaf964fd3
David <4292@holbertonschool.com>
2022-12-03 11:42:59 -0400

    nunmpy matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module to perform 2d matrix multiplication """
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Returns result of numpy array multiplication """
+
+    return np.matmul(mat1, mat2)
